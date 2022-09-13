d1da17a54675221337e19e5f0fa7ab00dc113ab0
Afooseyi <afooseyi@gmail.com>
2022-09-13 13:11:21 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet of letters
+ *  
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{	
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
