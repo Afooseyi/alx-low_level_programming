d432ec47ee83eb9003954e9352597ec1cbb4ecfc
Afooseyi <afooseyi@gmail.com>
2022-09-19 13:42:34 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * puts - prints a string
+ * _puts - prints a string
  * @str: string to print
  *
- * Description: prints a string 
+ * Description: prints a string
  * On success: returns no error
  */
 
@@ -13,9 +13,9 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (*(str + 1) != '\0')
+	while (*(str + i) != '\0')
 	{
-		putchar(*(str + 1));
+		putchar(*(str + i));
 		i++;
 	}
 	putchar(10);
