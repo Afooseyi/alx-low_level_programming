94fca729f9a2531463ffcbc20c65dbcb1fc9ff6b
Afooseyi <afooseyi@gmail.com>
2022-10-09 09:39:51 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = calloc(nmemb,  size);
+	ptr = calloc(nmemb, size);
 	if (ptr == NULL)
 		return (NULL);
 	return (ptr);
