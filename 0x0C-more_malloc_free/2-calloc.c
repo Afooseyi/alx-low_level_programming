408d4bb23b338a300653bc7415bbba6c6755e98e
Afooseyi <afooseyi@gmail.com>
2022-10-09 06:49:07 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,25 +6,17 @@
  ** @nmemb: number of elements.
  * @size: size of bytes.
  * Return: pointer to the allocated memory.
- * if nmemb or size is zero, returns NULL.
- * if malloc fails, returns NULL.
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+	int *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-
-	if (p == NULL)
+	ptr = calloc(nmemb,  size);
+	if (ptr == NULL)
 		return (NULL);
-
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
-
-		return (p);
+	return (ptr);
 }
