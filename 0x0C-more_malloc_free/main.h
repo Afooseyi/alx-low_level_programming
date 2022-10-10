c21cb54fe3d9d7d1ce9edc207cd8a264247b4d76
Afooseyi <afooseyi@gmail.com>
2022-10-10 12:51:01 -0700

    Task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <limits.h>
-int _putchar(char c);
+int _putchar(char);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
